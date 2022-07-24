23558341ba194aa2745a1530c011a0dc224cd4ab
Suraj Ghadge <sghadge@smartsight.in>
2022-07-24 19:56:22 +0530

    applied scale and opacity on list item

--- src/component/singleLineItem.js
+++ src/component/singleLineItem.js
@@ -1,12 +1,31 @@
 import {View, Text, TouchableOpacity} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  withSpring,
+} from 'react-native-reanimated';
 
 const SingleLineItem = ({data, onPress, styles}) => {
+  const progress = useSharedValue(0);
+
+  const reanimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: progress.value,
+      transform: [{scale: progress.value / 2}],
+    };
+  }, []);
+
+  useEffect(() => {
+    progress.value = withTiming(2, {duration: 1000});
+  });
+
   return (
     <TouchableOpacity onPress={() => onPress()}>
-      <View style={styles.item}>
+      <Animated.View style={[styles.item, reanimatedStyle]}>
         <Text style={styles.title}>{data.item.label}</Text>
-      </View>
+      </Animated.View>
     </TouchableOpacity>
   );
 };
